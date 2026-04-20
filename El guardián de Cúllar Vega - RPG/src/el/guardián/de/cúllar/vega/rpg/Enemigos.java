76c76cea7c81382c4025d162bb689708320bf8fb
pablosierra23 <pablosg1008@gmail.com>
2026-04-20 12:41:11 +0200

    Creo clase Enemigo

---
+++ El guardián de Cúllar Vega - RPG/src/el/guardián/de/cúllar/vega/rpg/Enemigos.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package el.guardián.de.cúllar.vega.rpg;
+
+/**
+ *
+ * @author EAG
+ */
+public class Enemigos extends Personaje{
+    //Atributos
+    private TipoDificultad dificultad;
+    private String tipo;
+}
+
+//Constructor por defecto
+
+
+//Constructor por parametros
+
+
+//Constructor de copia
+
+
+//Getters
+
+
+//Setters
+
+
+//ToString
