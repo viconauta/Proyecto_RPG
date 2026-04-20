8fe101bbdd447625d73d9fb8481509b431d925c4
viconauta <antonio-vico@hotmail.es>
2026-04-20 19:07:20 +0200

    añado el enum de dificultad

---
+++ El guardián de Cúllar Vega - RPG/src/el/guardián/de/cúllar/vega/rpg/TipoDificultad.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package el.guardián.de.cúllar.vega.rpg;
+
+/**
+ *
+ * @author anton
+ */
+public enum TipoDificultad {
+    Facil, Normal, Dificil
+}
